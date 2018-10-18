913a3af805266c12226dce9c17560770543b0744
JrNordell <nordejr@gmail.com>
2018-10-18 18:53:04 -0500

    ey yeet

--- myshell.c
+++ myshell.c
@@ -1,6 +1,6 @@
 /*
  * This code implements a simple shell program
- * It supports the internal shell command "exit", 
+ * It supports the internal shell command "exit",
  * backgrounding processes with "&", input redirection
  * with "<" and output redirection with ">".
  * However, this is not complete.
@@ -27,193 +27,193 @@ void sig_handler(int signal) {
 
 /*
  * The main shell function
- */ 
+ */
 main() {
-  int i;
-  char **args; 
-  int result;
-  int block;
-  int output;
-  int input;
-  int pipe;
-  char *output_filename;
-  char *input_filename;
-  char *pipe_command;
+    int i;
+    char **args;
+    int result;
+    int block;
+    int output;
+    int input;
+    int pipe;
+    char *output_filename;
+    char *input_filename;
+    char *pipe_command;
 
   // Set up the signal handler
 //  sigset(SIGCHLD,  sig_handler);
- 
+
 // Loop forever
-  while(1) {
-	int status;   
-	int pid =  waitpid(-1, &status, WNOHANG);
-	char* buff[100];
-    	getcwd(buff, 100);
+    while(1) {
+        int status;
+        int pid =  waitpid(-1, &status, WNOHANG);
+        char* buff[100];
+        getcwd(buff, 100);
 
-	if(pid>0){
-		printf("PID %d has finished\n", pid);
-	}else{
-    		char* buff[100];
-    		getcwd(buff, 100);
-    		// Print out the prompt and get the input
-    		printf("%s$ ", buff);
-    		args = getaline();
+        if(pid>0){
+            printf("PID %d has finished\n", pid);
+        }else{
+            char* buff[100];
+            getcwd(buff, 100);
+            // Print out the prompt and get the input
+            printf("%s$ ", buff);
+            args = getaline();
 
-    // No input, continue
-    if(args[0] == NULL)
-      continue;
+            // No input, continue
+            if(args[0] == NULL)
+                continue;
 
-    // Check for internal shell commands, such as exit
-    if(internal_command(args))
-      continue;
+            // Check for internal shell commands, such as exit
+            if(internal_command(args))
+                continue;
 
-    	int index = 0;
-	int numPipes = 0;
-	int cmdcount=0;
-	char*** cmd[10];
-	cmd[cmdcount] = &args[0];
-	cmdcount++;
-	while(args[index] != NULL){
-		if(args[index][0] == ';'){
-			cmd[cmdcount] = &args[index+1];
-			free(args[index]);	
-			args[index] = NULL;
-			cmdcount++;
-		}		
-		index++;
-	}
+            int index = 0;
+            int numPipes = 0;
+            int cmdcount=0;
+            char*** cmd[10];
+            cmd[cmdcount] = &args[0];
+            cmdcount++;
+            while(args[index] != NULL){
+                if(args[index][0] == ';'){
+                    cmd[cmdcount] = &args[index+1];
+                    free(args[index]);
+                    args[index] = NULL;
+                    cmdcount++;
+                }
+                index++;
+            }
 
-	int j;
-	for(j=0;j < cmdcount; j++){
-	printf("COMMAND: %s\n", *cmd[j]);
-	args = cmd[j];		
+            int j;
+            for(j=0;j < cmdcount; j++){
+                printf("COMMAND: %s\n", *cmd[j]);
+                args = cmd[j];
 
-    // Check for an ampersand
-    block = ampersand(args);
+                // Check for an ampersand
+                block = ampersand(args);
 
-    // Check for redirected input
-    input = redirect_input(args, &input_filename);
+                // Check for redirected input
+                input = redirect_input(args, &input_filename);
 
-    switch(input) {
-    case -1:
-      printf("Syntax error!\n");
-      continue;
-      break;
-    case 0:
-      break;
-    case 1:
-      printf("Redirecting input from: %s\n", input_filename);
-      break;
+                switch(input) {
+                case -1:
+                    printf("Syntax error!\n");
+                    continue;
+                    break;
+                case 0:
+                    break;
+                case 1:
+                    printf("Redirecting input from: %s\n", input_filename);
+                    break;
+                }
+
+                 // Check for redirected output
+                output = redirect_output(args, &output_filename);
+
+                switch(output) {
+                    case -1:
+                        printf("Syntax error!\n");
+                        continue;
+                        break;
+                    case 0:
+                        break;
+                    case 1:
+                        printf("Redirecting output to: %s\n", output_filename);
+                        break;
+                    case 2:
+                        printf("Redirecting >> output to: %s\n", output_filename);
+                        break;
+                }
+
+                //Check for pipe
+                //pipe = redirect_pipe(args, &pipe_command);
+
+                index = 0;
+                int pipeCount = 0;
+                while(args[index] != NULL){
+                    if(args[index][0] == '|'){
+                        cmd[pipeCount] = &args[index+1];
+                        free(args[index]);
+                        args[index] = NULL;
+                        pipeCount++;
+                    }
+                    index++;
+                }
+
+
+
+                // Do the command
+                do_command(args, block,
+                       input, input_filename,
+                        output, output_filename);
+            }
+        }
     }
-
-    // Check for redirected output
-    output = redirect_output(args, &output_filename);
-
-    switch(output) {
-    case -1:
-      printf("Syntax error!\n");
-      continue;
-      break;
-    case 0:
-      break;
-    case 1:
-      printf("Redirecting output to: %s\n", output_filename);
-      break;
-    case 2:
-      printf("Redirecting >> output to: %s\n", output_filename);
-      break;
-    }
-
-    //Check for pipe
-//    pipe = redirect_pipe(args, &pipe_command);
-
-	index = 0;
-	int pipeCount = 0;
-	while(args[index] != NULL){
-		if(args[index][0] == '|'){
-			cmd[pipeCount] = &args[index+1];
-			free(args[index]);	
-			args[index] = NULL;
-			pipeCount++;
-		}		
-		index++;
-	}
-
-
-
-    // Do the command
-    do_command(args, block, 
-	       input, input_filename, 
-	       output, output_filename);
-  }
-}
-}
 }
 
 /*
  * Check for ampersand as the last argument
  */
 int ampersand(char **args) {
-  int i;
+    int i;
 
-  for(i = 1; args[i] != NULL; i++) ;
-  if(args[i-1][0] == '&') {
-    free(args[i-1]);
-    args[i-1] = NULL;
-    return 0;
-  }
-  
-  return 1;
+    for(i = 1; args[i] != NULL; i++) ;
+    if(args[i-1][0] == '&') {
+        free(args[i-1]);
+        args[i-1] = NULL;
+        return 0;
+    }
+
+    return 1;
 }
 
-/* 
+/*
  * Check for internal commands
- * Returns true if there is more to do, false otherwise 
+ * Returns true if there is more to do, false otherwise
  */
 int internal_command(char **args) {
-  if(strcmp(args[0], "exit") == 0) {
-    exit(0);
-  }else if(strcmp(args[0], "cd") == 0){
-    chdir(args[1]);
-  }
+    if(strcmp(args[0], "exit") == 0) {
+        exit(0);
+    }else if(strcmp(args[0], "cd") == 0){
+        chdir(args[1]);
+    }
 
-  return 0;
+    return 0;
 }
 
-/* 
+/*
  * Do the command
  */
 int do_command(char **args, int block,
-	       int input, char *input_filename,
-	       int output, char *output_filename) {
-  
-  int result;
-  pid_t child_id;
-  int status;
-  
-// Fork the child process
-  child_id = fork();
+        int input, char *input_filename,
+        int output, char *output_filename) {
 
-  // Check for errors in fork()
-  switch(child_id) {
-  case EAGAIN:
-    perror("Error EAGAIN: ");
-    return;
-  case ENOMEM:
-    perror("Error ENOMEM: ");
-    return;
-  }
- if(child_id == 0) {
-	if(!block)
-	 setpgid(child_id, 0);
-	 // Set up redirection in the child process
-    if(input)
-      freopen(input_filename, "r", stdin);
+    int result;
+    pid_t child_id;
+    int status;
 
-    if(output==1)
-      freopen(output_filename, "w+", stdout);
-    if(output==2)
-      freopen(output_filename, "a", stdout);
+    // Fork the child process
+    child_id = fork();
+
+    // Check for errors in fork()
+    switch(child_id) {
+        case EAGAIN:
+            perror("Error EAGAIN: ");
+            return;
+        case ENOMEM:
+            perror("Error ENOMEM: ");
+            return;
+    }
+    if(child_id == 0) {
+        if(!block)
+            setpgid(child_id, 0);
+            // Set up redirection in the child process
+        if(input)
+            freopen(input_filename, "r", stdin);
+
+        if(output==1)
+            freopen(output_filename, "w+", stdout);
+        if(output==2)
+            freopen(output_filename, "a", stdout);
 
 //	printf("%s\n", args[0]);
 //	printf("%s\n", args[1]);
@@ -222,21 +222,21 @@ int do_command(char **args, int block,
 //      printf("Args 0 = %s\n", args[0]);
 //      printf("Args 2 = %s\n", args[2]);
 //      result = execvp(args[0], args);
-//    }else{	
+//    }else{
       // Execute the command
-      
-      result = execvp(args[0], args);
+
+        result = execvp(args[0], args);
 //    }
-   exit(-1);
-  }
+        exit(-1);
+    }
 
   // Wait for the child process to complete, if necessary
-  if(block) {
-    printf("Waiting for child, pid = %d\n", child_id);
-    result = waitpid(child_id, &status, 0);
-   }else{		
-       printf("%d started in background\n", child_id);
-   }
+    if(block) {
+        printf("Waiting for child, pid = %d\n", child_id);
+        result = waitpid(child_id, &status, 0);
+    }else{
+        printf("%d started in background\n", child_id);
+    }
 }
 
 /*
@@ -279,15 +279,15 @@ int redirect_output(char **args, char **output_filename) {
   int j;
 
   for(i = 0; args[i] != NULL; i++) {
-	
+
     // Look for the >
     if(args[i][0] == '>') {
-     	//Look for the second > 
+     	//Look for the second >
      if(args[i+1][0] == '>'){
 	free(args[i]);
 	free(args[i+1]);
-	 
-	// Get the filename 
+
+	// Get the filename
       	if(args[i+2] != NULL) {
 		*output_filename = args[i+2];
       	} else {
@@ -301,10 +301,10 @@ int redirect_output(char **args, char **output_filename) {
 
 	return 2;
       }else{
-      //Only one > was found 
+      //Only one > was found
 	free(args[i]);
 
-      // Get the filename 
+      // Get the filename
       if(args[i+1] != NULL) {
 	*output_filename = args[i+1];
       } else {
